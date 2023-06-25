8b4da5164810943891bf5e6e708c44faf120ad60
Andrei_W10_2016 <playdota3@gmail.com>
2023-06-26 00:14:19 +0300

    active tabs in templates

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,8 +1,8 @@
-from django.template import RequestContext
+# from django.template import RequestContext
 from django.template.context_processors import request
 
 from blog.models import Category
 
 
 def category_context(request):
-    return {'categories': Category.objects.all()}
+    return {'categories': Category.objects.all(), 'path': request.path}
